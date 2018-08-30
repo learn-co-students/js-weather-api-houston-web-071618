56e5aab1138f084b03303f15be879c61af1e15b6
Christopher Arnold <chrisarnold2010@gmail.com>
2018-08-30 15:25:37 -0500

    chart complete

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const ctx = document.getElementById("NYCWeatherChart").getContext("2d")
-  const API_KEY = "Go get an API key";
+  const API_KEY = "308435004fc27a21a03ed45a253e2210";
   const CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
   const URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/40.7127,-74.0059?exclude=currently,minutely,daily";
 
