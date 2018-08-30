c755c9306a4b7483d5186b643efc31368730843a
Sean Huang <hyprul@ucla.edu>
2018-08-30 14:42:17 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,9 +1,49 @@
-function getFahrenheit(result){
-  // Your code goes here
+// function getFahrenheit(result){
+//   // Your code goes here
+//   var newArr = []
+//   result.hourly.data.forEach(function(element){
+//     newArr.push(element.temperature)
+
+//   })
+    
+//       return newArr
+
+// }
+
+function getFahrenheit(result) {
+ // Your code goes here
+ return result.hourly.data.map(element => {
+   return element.temperature;
+ });
 }
 
-function getHour(result){
-  // Your code goes here
+// function getHour(result){
+//   // Your code goes here
+
+//   var newArr = []
+//   result.hourly.data.forEach(function(element){
+
+
+//   let myDate = new Date(element.time * 1000)
+
+
+//   newArr.push(myDate.getHours())
+
+
+
+// })
+//   return newArr
+
+// }
+
+function getHour(result) {
+ // Your code goes here
+
+ return result.hourly.data.map(function(element) {
+   let myDate = new Date(element.time * 1000);
+   return myDate.getHours();
+ });
+ return newArr;
 }
 
 function generateDataSet(labels, data) {
@@ -28,7 +68,15 @@ function generateDataSet(labels, data) {
 
 function makeRequest(endpoint, canvas) {
   // Your code goes here
+  fetch(endpoint)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(hourlyData){
+    console.log(hourlyData)
+    const tempChart = new Chart(canvas, generateDataSet(getHour(hourlyData), getFahrenheit(hourlyData)))
+  })
+
 
   // After your fetch works - use your json data with the line below :)
-  // const tempChart = new Chart(canvas, generateDataSet(getHour(hourlyData), getFahrenheit(hourlyData)))
 }
