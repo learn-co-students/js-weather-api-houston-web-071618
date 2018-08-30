1029c256e80d1a3dee87c38f9f28937457d88369
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-08-30 16:49:08 -0500

    Done.

--- js/init.js
+++ js/init.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   const ctx = document.getElementById("NYCWeatherChart").getContext("2d")
-  const API_KEY = "Go get an API key";
+  const API_KEY = "388b98e6a2eb275b0f855b8a8892929f";
   const CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  const URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/40.7127,-74.0059?exclude=currently,minutely,daily";
+  const URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/29.7575,-95.3668?exclude=currently,minutely,daily";
+
 
   // make your fetch in the makeRequest function in weatherChart.js
   makeRequest(URL, ctx)
